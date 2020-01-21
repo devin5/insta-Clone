f7420f68a893f22af38709356089fc21b58d597e
Devin Dias <devindias.tech@gmail.com>
2020-01-21 06:10:57 -0600

    initialized project
    adds assetts folder
    creates the react shell

---
+++ src/App.js
@@ -0,0 +1,21 @@
+/* 
+Start here and then work through the 
+PostsContainer components and the CommentSection Components. 
+Not all files need code added. 
+Look at each file to see where you need to pass props or add code 
+*/
+import React from "react";
+import "./App.css";
+// import the PostsPage and SearchBar and add them to the App
+// import dummydata
+
+const App = () => {
+  // set up state for the dummy data and pass to your PostsPage
+  return (
+    <div className="App">
+      {/* Add components here  and pass props where appropriate */}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/PostsContainer/PostsPage.js
@@ -0,0 +1,16 @@
+//Complete the necessary code in this file
+import React from "react";
+import Post from "./Post";
+import "./Posts.css";
+
+// pass the data from App.js down as props then map through the data
+const PostsPage = () => {
+  return (
+    <div className="posts-container-wrapper">
+      {/* map through data here */}
+    </div>
+  );
+};
+
+export default PostsPage;
+
